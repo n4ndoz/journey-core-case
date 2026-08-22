e1ee5220bd1ee7bf2c779b248f6870187298597d
Fernando Limoeiro Lara <fll.oliveira.c2@gmail.com>
2026-08-22 13:40:11 -0300

    feat: add domain model enums

---
+++ app/domain/enums.py
@@ -0,0 +1,16 @@
+from enum import StrEnum
+
+
+class ProtocolSessionStatus(StrEnum):
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+
+class JourneyStatus(StrEnum):
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+
+class TaskStatus(StrEnum):
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
